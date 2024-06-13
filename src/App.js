188e41d3dc3f824266f5430a5998918cf3ca3779
aalmogg <aalmogg@gmail.com>
2024-06-13 15:00:35 +0300

    react project init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Counter from './components/Counter';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+function NavigationBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={NavLink} to="/">My React Starter</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/counter">Counter</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
